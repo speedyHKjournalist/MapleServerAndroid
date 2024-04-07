db542d2f693b28c70e0a2909ad0d053c322bc0f3
speedyHKjournalist <xingbizhou@gmail.com>
2024-04-07 16:12:52 +0800

    UI Bug fix.

--- app/src/main/java/com/mapleserver/ServerService.kt
+++ app/src/main/java/com/mapleserver/ServerService.kt
@@ -1,9 +1,15 @@
 package com.mapleserver
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
+import androidx.activity.ComponentActivity
+import androidx.core.app.NotificationCompat
 import net.server.Server
 
 
@@ -34,17 +40,42 @@ class ServerService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        startMapleService()
+        startMapleService(intent, startId)
         return START_NOT_STICKY
     }
 
-    fun startMapleService() {
+    fun startMapleService(intent: Intent?, startId: Int) {
         if (!isRunning()) {
+            val channelId = intent?.getStringExtra("channel_id") ?: "default_id"
+            val pendingIntent : PendingIntent?
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                pendingIntent = intent?.getParcelableExtra("pending_intent", PendingIntent::class.java)
+            } else {
+                pendingIntent = intent?.getParcelableExtra<PendingIntent>("pending_intent")
+            }
+
             notifyServerStarting()
+
             val initializationThread = Thread {
+                val notificationManager = getSystemService(ComponentActivity.NOTIFICATION_SERVICE) as NotificationManager
+                val channel = NotificationChannel(channelId, "OpenMapleServer Channel", NotificationManager.IMPORTANCE_DEFAULT)
+                notificationManager.createNotificationChannel(channel)
+
+                val builder = NotificationCompat.Builder(this, "1")
+                builder.setSmallIcon(R.drawable.ic_launcher)
+                    .setContentTitle("OpenMapleServer Notification")
+                    .setContentText("OpenMapleServer is Running")
+                    .setContentIntent(pendingIntent)
+                    .setChannelId("1")
+                    .setAutoCancel(true)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                val notification = builder.build()
+                startForeground(startId, notification)
+
                 val args = arrayOf("-Xmx2048m", "-Dwz-path=wz", "-Djava.net.preferIPv4Stack=true")
                 Server.getInstance(applicationContext)
                 Server.main(args)
+
                 notifyServerStarted()
             }
             initializationThread.start()
